3e5cb93856cd21c1ae83f54ced0dd835f5608a4c
yxsavior <220849743+yxsavior@users.noreply.github.com>
2026-07-19 22:18:32 -0300

    feat: add SQL solution for 1280 Students and Examinations

---
+++ 02-basic-joins/1280-students-and-examinations/solution.sql
@@ -0,0 +1,19 @@
+-- Write a solution to find the number of times each student attended each exam.
+-- Return the result table ordered by student_id and subject_name.
+
+SELECT 
+    st.student_id, 
+    st.student_name, 
+    s.subject_name, 
+    COUNT(e.subject_name) AS attended_exams 
+FROM Students st
+CROSS JOIN Subjects s
+LEFT JOIN Examinations e
+    ON st.student_id = e.student_id
+    AND s.subject_name = e.subject_name
+GROUP BY 
+    st.student_id, 
+    st.student_name, 
+    s.subject_name
+ORDER BY 
+    st.student_id;
