ee6db29ade5882d2ec70719175f4e7ac7e2efc6b
yxsavior <220849743+yxsavior@users.noreply.github.com>
2026-07-19 22:17:46 -0300

    feat: add SQL solution for 1581 Customer Who Visited but Did Not Make Any Transactions

---
+++ 02-basic-joins/1581-customer-who-visited-but-did-not-make-any-transactions/solution.sql
@@ -0,0 +1,11 @@
+-- Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+-- Return the result table sorted in any order.
+
+SELECT 
+    v.customer_id, 
+    COUNT(*) as count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+    ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
